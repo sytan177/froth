9c4acb9869a38ea8db7e75192075315f022ad795
Shuyu Tan <floatingair10@gmail.com>
2026-02-23 11:07:59 +0100

    add package_data to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name="froth",
     version="0.1.0",
     packages=find_packages(),
+    package_data={'froth': ['data/**/*', 'data/*']},
+    include_package_data=True,
     install_requires=[
         "numpy>=1.26",
         "torch>=2.7",
